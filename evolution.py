24b1dd86f3a34c272968a59a643b61c00c687953
Babaiko124 <babaiko24@gmail.com>
2023-11-27 21:53:14 +0700

    s

---
+++ evolution.py
@@ -0,0 +1,9 @@
+# Задание №2
+print("Введите по порядку этапы развития человека.")
+stage1=input("Первый этап ")
+stage2=input("Второй этап ")
+stage3=input("Третий этап ")
+stage4=input("Четвертый этап ")
+stage5=input("Пятый этап ")
+stage6=input("Шестой этап ")
+print(stage1,stage2,stage3,stage4,stage5,stage6,sep="=>")
---
+++ Урок 3.py
@@ -0,0 +1,15 @@
+# Задание №1
+a=input ("Вид питомца ")
+b=input ("Его возраст ")
+c=input ("Кличка ")
+print("Это",a,"по кличке",'"',c,'". '"Возраст:",b,"года")
+
+# Задание №2
+print("Введите по порядку этапы развития человека.")
+stage1=input("Первый этап ")
+stage2=input("Второй этап ")
+stage3=input("Третий этап ")
+stage4=input("Четвертый этап ")
+stage5=input("Пятый этап ")
+stage6=input("Шестой этап ")
+print(stage1,stage2,stage3,stage4,stage5,stage6,sep="=>")
