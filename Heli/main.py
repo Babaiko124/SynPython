e39e6bd0b881c32f3be60713fbfafbf5a7d13ee2
Babaiko124 <babaiko24@gmail.com>
2023-12-21 01:48:30 +0700

    update

--- Heli/main.py
+++ Heli/main.py
@@ -7,7 +7,7 @@ from pynput import keyboard
 from clouds import Clouds
 
 
-TICK_SLEEP =10
+TICK_SLEEP =0.05
 TREE_UPDATE = 50
 CLOUDS_UPDATE = 100
 FIRE_UPDATE = 100
@@ -22,17 +22,25 @@ tick = 1
 MOVES = {'w': (-1,0), 'd': (0, 1), 's': (1,0), 'a': (0,-1)}
 # f - сохранение, g - загрузка
 def process_key(key):
-    global heli
+    global heli, tick, clouds, field
     c = key.char.lower()
     if c in MOVES.keys():
         dx, dy = MOVES[c][0], MOVES[c][1]
         heli.move(dx,dy)
-    if c == 'f':
+    elif c == 'f':
         data = {'helicopter': heli.export_data(),
                 'clouds': clouds.export_data(),
-                'field': field.export_data()}
+                'field': field.export_data(),
+                'tick': tick}
         with open('level.json','w') as lvl:
             json.dump(data, lvl)
+    elif c == 'g':
+        with open('level.json', 'r') as lvl:
+            data = json.load(lvl)
+            heli.import_data(data['helicopter'])
+            tick = data['tick'] or 1
+            field.import_data(data['field'])
+            clouds.import_data(data['clouds'])
         
 listener = keyboard.Listener(
     on_press=None,
